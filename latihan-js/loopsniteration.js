7b5454dcbe9057ee84e91c4e365e1f03d7c58087
ayyubnurumi <ayyubinvestment@gmail.com>
2022-07-12 17:18:00 +0700

    latihan looping dan iteration js

---
+++ latihan-js/loopsniteration.js
@@ -0,0 +1,30 @@
+let i = 0;
+do {
+  i += 1;
+  console.log(i);
+} while (i < 5);
+
+let n = 0;
+let x = 0;
+while (n < 3) {
+  n++;
+  x += n;
+} 
+console.log(n)
+  
+let x = 0;
+let z = 0;
+labelCancelLoops: while (true) {
+  console.log('anjayy: ' + x);
+  x += 1;
+  z = 1;
+  while (true) {
+    console.log('wkwkwk: ' + z);
+    z += 1;
+    if (z === 8 && x === 8) {
+      break labelCancelLoops;
+    } else if (z === 8) {
+      break;
+    }
+  }
+}
