c2d8ea4bdf39bf01013a9a9f37598eae73afdad1
ayyubnurumi <ayyubinvestment@gmail.com>
2022-08-26 09:42:23 +0700

    praxis

--- front-end/week-1/day-1/case/looping.js
+++ front-end/week-1/day-1/case/looping.js
@@ -1,27 +1,25 @@
-for (let line = "#"; line.length <8;
-    line += "#")
-    console.log(line)
+for (let line = "#"; line.length < 8; line += "#") console.log(line);
 
-let abc = "abc"
-    console.log(abc.length)
+let abc = "abc";
+console.log(abc.length);
 
-for (let n = 1; n <=100; n++) {
-    let output = "";
-    if (n % 3 == 0) output += "Fizz";
-    if (n % 5 == 0) output += "Buzz";
-    console.log(output || n);
+for (let n = 1; n <= 100; n++) {
+  let output = "";
+  if (n % 3 == 0) output += "Fizz";
+  if (n % 5 == 0) output += "Buzz";
+  console.log(output || n);
 }
 
 let size = 8;
 let board = "";
 for (let y = 0; y < size; y++) {
-    for (let x = 0; x < size; x++) {
-        if ((x + y) % 2 == 0) {
-            board += " ";
-        } else {
-            board += "#";
-        }
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
     }
-    board += "\n"
+  }
+  board += "\n";
 }
 console.log(board);
