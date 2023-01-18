3ff19edc9eda02129b52b215f52c83eed9d5c640
ayyubnurumi <ayyubinvestment@gmail.com>
2023-01-18 16:50:09 +0700

    praxis

--- front-end/week-1/day-1/case/index.js
+++ front-end/week-1/day-1/case/index.js
@@ -1,20 +1,68 @@
-let nomer = 1
-nomer = 2
+let nomer = 1;
+nomer = 2;
 
-let string = `apa`
-let integer = 3
-let boolean = false
-let undifined = undefined
-let isNan = NaN
-let isNull = null
-let symbol1 = Symbol('simbol')
-let symbol2 = Symbol('simbol')
+let string = `apa`;
+let integer = 3;
+let boolean = false;
+let undifined = undefined;
+let isNan = NaN;
+let isNull = null;
+let symbol1 = Symbol("simbol");
+let symbol2 = Symbol("simbol");
 
-let array = []
-let object = {}
+let array = [{}, "data", {}];
+let object = {
+  "name bauh": "jeruk",
+  rasa: "mangga",
+  warna: "pink",
+  harga: [{ kecil: 1000 }, { sedeng: 5000 }, { gede: 10000 }],
+};
 
-let a = 10
-let b = 3
-let c = 4
-let d = a / 5
-console.log(typeof isNan);
+let a = 10;
+let b = 3;
+let c = 4;
+let d = a / 5;
+
+let hujan = true;
+let sehat = true;
+let restuOrangTua = false;
+sehat
+  ? hujan
+    ? restuOrangTua
+      ? console.log("hujan-hujanan")
+      : console.log("ngaji dirumah")
+    : console.log("jalan-jalan")
+  : console.log("tidur");
+
+if (sehat) {
+  if (hujan) {
+    if (restuOrangTua) {
+      console.log("hujan-hujanan");
+    } else {
+      console.log("ngaji dirumah");
+    }
+  } else {
+    console.log("jalan-jalan");
+  }
+} else {
+  console.log("tidur");
+}
+
+let weather = "rainy";
+if (weather == "rainy") {
+  console.log("You need a rain coat.");
+} else if (weather == "cloudy") {
+  console.log("It might be cold, you need a jacket.");
+} else if (weather == "sunny") {
+  console.log("Go out freely.");
+} else {
+  console.log("No need for rain coat.");
+}
+
+weather == "rainy"
+  ? console.log("You need a rain coat.")
+  : weather == "cloudy"
+  ? console.log("It might be cold, you need a jacket.")
+  : weather == "sunny"
+  ? console.log("Go out freely.")
+  : console.log("No need for rain coat.");
